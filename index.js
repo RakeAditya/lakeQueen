d9af989e5d99ff08e4b2c35c45b6c46e3157de4d
RakeAditya <aditya01gkl@gmail.com>
2023-10-25 14:41:20 +0530

    new bug fixes

--- index.js
+++
@@ -1,7 +0,0 @@
-// menu toggle bar
-const menuToggle = document.getElementById('menu-toggle');
-const mobileMenu = document.getElementById('mobile-menu');
-
-menuToggle.addEventListener('click', function () {
-	mobileMenu.classList.toggle('active');
-});
